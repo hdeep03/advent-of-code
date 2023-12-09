baa70f026d8e365faffe7d68f6e1d5b81e307114
hdeep03 <hdeep03@gmail.com>
2023-12-08 21:17:42 -0800

    Day 9

---
+++ day-09/a.py
@@ -0,0 +1,24 @@
+import sys
+
+def main():
+    data = sys.stdin.read()
+    data = data.splitlines()
+    x = 0
+    for line in data:
+        x+= (process(line))
+    print(x)
+
+def process(line: str) -> int:
+    starting_elements = [int(x) for x in line.split(' ')]
+    layers = [starting_elements]
+    while any(layers[-1]):
+        new_layer = []
+        for i in range(1, len(layers[-1])):
+            new_layer.append(layers[-1][i] - layers[-1][i-1])
+        layers.append(new_layer)
+    prop = 0
+    for i in range(len(layers) - 1, -1, -1):
+        prop += layers[i][-1]
+    return prop
+
+main()
